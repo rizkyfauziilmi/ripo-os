a410f4b358ae6fbbc18d777f5bc6f50ff0bd5860
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2025-05-05 22:52:32 +0700

    add default wallpaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Topbar from "./components/topbar/topbar";
 import { ThemeProvider } from "./components/theme-provider";
 import Dock from "./components/dock/dock";
+import bgImage from "./assets/wallpaper-1.png";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="bg-muted h-screen w-screen">
+      <div
+        className="bg-cover bg-muted h-screen w-screen"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
         <div className="h-full flex flex-col">
           <Topbar />
           <Dock />
