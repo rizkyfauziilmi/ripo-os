188e567f41683a57a2b6a74e8b227d92a9f65cc6
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2025-05-05 22:43:12 +0700

    Add DockAppIcon component for better dock styling

---
+++ src/components/dock/dock-app-icon.tsx
@@ -0,0 +1,34 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { JSX } from "react";
+import { Button } from "../ui/button";
+
+interface DockAppIconProps {
+  app: string;
+  icon: () => JSX.Element;
+}
+
+function DockAppIcon({ app, icon }: DockAppIconProps) {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button size="icon" className="size-12 cursor-pointer" variant="ghost">
+          {icon()}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent
+        side="right"
+        withArrow={false}
+        align="center"
+        sideOffset={15}
+      >
+        <p className="text-sm">{app}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
+
+export default DockAppIcon;
--- src/components/dock/dock.tsx
+++ src/components/dock/dock.tsx
@@ -7,12 +7,8 @@ import Spotify from "../icons/spotify";
 import WhatsApp from "../icons/whatsapp";
 import ZenBrowser from "../icons/zen";
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import DockAppIcon from "./dock-app-icon";
 
 // interface DockProps {
 //   side?: "left" | "right" | "bottom";
@@ -21,33 +17,21 @@ import {
 
 function Dock() {
   const icon: Map<string, () => JSX.Element> = new Map([
-    ["zen", () => <ZenBrowser className="size-10 cursor-pointer" />],
-    ["alacritty", () => <Alacritty className="size-10 cursor-pointer" />],
-    ["github", () => <GitHub className="size-10 cursor-pointer" />],
-    ["discord", () => <Discord className="size-10 cursor-pointer" />],
-    ["whatsapp", () => <WhatsApp className="size-10 cursor-pointer" />],
-    ["spotify", () => <Spotify className="size-10 cursor-pointer" />],
-    ["gmail", () => <Gmail className="size-10 cursor-pointer" />],
+    ["zen", () => <ZenBrowser className="size-8" />],
+    ["alacritty", () => <Alacritty className="size-8" />],
+    ["github", () => <GitHub className="size-8" />],
+    ["discord", () => <Discord className="size-8" />],
+    ["whatsapp", () => <WhatsApp className="size-8" />],
+    ["spotify", () => <Spotify className="size-8" />],
+    ["gmail", () => <Gmail className="size-8" />],
   ]);
 
   return (
     <div className="flex items-center w-fit flex-1">
-      <div className="ml-2 flex flex-col justify-center gap-6 px-2 rounded-md py-4 bg-background/80">
+      <div className="ml-2 flex flex-col justify-center gap-4 px-2 rounded-md py-4 bg-background/80">
         <TooltipProvider>
           {Array.from(icon).map(([key, IconComponent]) => (
-            <Tooltip key={key}>
-              <TooltipTrigger>
-                <IconComponent />
-              </TooltipTrigger>
-              <TooltipContent
-                side="right"
-                withArrow={false}
-                align="center"
-                sideOffset={15}
-              >
-                <p className="text-sm">{key}</p>
-              </TooltipContent>
-            </Tooltip>
+            <DockAppIcon app={key} icon={IconComponent} />
           ))}
         </TooltipProvider>
       </div>
